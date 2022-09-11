31eb7dc394972b2dd43d407250552c5032e98f7c
yeonsuBeak <soothbugger@gmail.com>
2022-09-11 16:43:14 +0900

    Test: Change an image to a captured copy

--- test/save-text/main.js
+++ test/save-text/main.js
@@ -2,3 +2,36 @@ function resize(obj) {
   obj.style.height = "1px";
   obj.style.height = 0 + obj.scrollHeight + "px";
 }
+
+const downloadButton = document.querySelector(".download-button");
+const captureModal = document.querySelector(".capture-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const captureExport = function () {
+  console.log("실행");
+  html2canvas(document.querySelector("#text-section"), {
+    logging: true,
+    letterRendering: 1,
+    allowTaint: true,
+    useCORS: true,
+  }).then((canvas) => {
+    captureModal.appendChild(canvas).classList.add("canvas");
+  });
+
+  modal.classList.remove("hidden");
+};
+
+const removeCapture = function () {
+  captureModal.removeChild(captureModal.firstElementChild);
+
+  modal.classList.add("hidden");
+};
+
+downloadButton.addEventListener("click", captureExport);
+overlay.addEventListener("click", removeCapture);
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    removeCapture();
+  }
+});
