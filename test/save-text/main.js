73783bbf000a3ef9dc08f95472ff00b98e0237a6
yeonsuBeak <soothbugger@gmail.com>
2022-09-17 11:52:41 +0900

    Post how to use html2canvas

--- test/save-text/main.js
+++ test/save-text/main.js
@@ -1,12 +1,24 @@
-function resize(obj) {
-  obj.style.height = "1px";
-  obj.style.height = 0 + obj.scrollHeight + "px";
-}
-
+const textSection = document.querySelector(".text-section");
+const textInput = document.querySelector(".text-input");
+const textPlaceholder = document.querySelector(".text-placeholder");
 const downloadButton = document.querySelector(".download-button");
 const captureModal = document.querySelector(".capture-modal");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+
+const resize = (e) => {
+  if (textInput) {
+    textInput.style.height = "auto";
+    let height = textInput.scrollHeight;
+    textInput.style.height = `${height}px`;
+  }
+
+  if (textInput.childNodes.length != 0) {
+    textPlaceholder.classList.add("hidden");
+  } else {
+    textPlaceholder.classList.remove("hidden");
+  }
+};
 
 const captureExport = function () {
   console.log("실행");
@@ -17,6 +29,10 @@ const captureExport = function () {
     useCORS: true,
   }).then((canvas) => {
     captureModal.appendChild(canvas).classList.add("canvas");
+    let el = document.createElement("a");
+    el.href = canvas.toDataURL("image/jpeg");
+    el.download = "letter.jpg";
+    el.click();
   });
 
   modal.classList.remove("hidden");
