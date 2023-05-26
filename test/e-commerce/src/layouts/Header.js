14c5336ecd8f7f7eced135dcb5ce7e2c28db6ed5
yeonsuBaek <soothbugger@gmail.com>
2023-05-26 13:41:39 +0900

    feat: 관리자 로그인/로그아웃 기능 구현

--- test/e-commerce/src/layouts/Header.js
+++ test/e-commerce/src/layouts/Header.js
@@ -2,16 +2,34 @@ import React from 'react';
 import logo from '../assets/icons/logo.svg';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import useAdminStore from '../stores/admin';
+import user from '../assets/icons/user.svg';
 
 const Header = () => {
+  const { adminID, setAdminID, setAdminPW } = useAdminStore();
+
+  const handleLogout = () => {
+    setAdminID('');
+    setAdminPW('');
+    localStorage.setItem('adminID', JSON.stringify(adminID));
+  };
+
   return (
     <header className={styles.header}>
       <Link className={styles.logo} to='/'>
         <img className={styles.logoImg} src={logo} alt='아이디어스' />
       </Link>
-      <Link to='/login' className={styles.login}>
-        로그인
-      </Link>
+
+      {JSON.parse(localStorage.getItem('adminID')) ===
+      `${process.env.REACT_APP_ADMIN_ID}` ? (
+        <button type='button' onClick={handleLogout}>
+          <img src={user} alt='관리자 메뉴 열기' />
+        </button>
+      ) : (
+        <Link to='/login' className={styles.login}>
+          로그인
+        </Link>
+      )}
     </header>
   );
 };
--- test/e-commerce/src/pages/Login.js
+++ test/e-commerce/src/pages/Login.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/icons/logo.svg';
+import useAdminStore from '../stores/admin';
 
 const Login = () => {
+  const { adminID, adminPW, setAdminID, setAdminPW } = useAdminStore();
+  const navigate = useNavigate();
+
+  const handleChangeId = (e) => {
+    setAdminID(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setAdminPW(e.target.value);
+  };
+
+  const handleSubmitAdmin = (e) => {
+    e.preventDefault();
+
+    if (
+      adminID === `${process.env.REACT_APP_ADMIN_ID}` &&
+      adminPW === `${process.env.REACT_APP_ADMIN_PASSWORD}`
+    ) {
+      localStorage.setItem('adminID', JSON.stringify(adminID));
+      navigate('/');
+    } else {
+      alert('아이디 또는 비밀번호가 잘못되었습니다.');
+      setAdminID('');
+      setAdminPW('');
+    }
+  };
+
   return (
     <>
       <Link to='/'>
@@ -15,9 +43,9 @@ const Login = () => {
       <button type='button'>구글 3초만에 시작하기</button>
 
       <h2>관리자 전용 로그인</h2>
-      <form>
-        <input type='text' />
-        <input type='password' />
+      <form onSubmit={handleSubmitAdmin}>
+        <input type='text' onChange={handleChangeId} />
+        <input type='password' onChange={handleChangePassword} />
         <button type='submit'>로그인</button>
       </form>
     </>
