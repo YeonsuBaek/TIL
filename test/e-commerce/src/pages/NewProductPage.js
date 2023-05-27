e282b1b24991a3eb5ea026fff81394b19801df0d
yeonsuBaek <soothbugger@gmail.com>
2023-05-27 14:18:23 +0900

    feat: 관리자 외 상품 등록 페이지 접근 시 경고

--- test/e-commerce/src/pages/NewProductPage.js
+++ test/e-commerce/src/pages/NewProductPage.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NewProductForm from '../components/NewProductForm';
 import Header from '../layouts/Header';
+import useAdminStore from '../stores/admin';
 
 const NewProductPage = () => {
+  const { adminID } = useAdminStore();
+
   return (
     <>
       <Header />
-      <h2>상품 등록하기</h2>
-      <NewProductForm />
+      {adminID === `${process.env.REACT_APP_ADMIN_ID}` ? (
+        <>
+          <h2>상품 등록하기</h2>
+          <NewProductForm />
+        </>
+      ) : (
+        <div>
+          로그인 후 이용해주세요.
+          <Link to='/login'>로그인하러 가기</Link>
+        </div>
+      )}
     </>
   );
 };
